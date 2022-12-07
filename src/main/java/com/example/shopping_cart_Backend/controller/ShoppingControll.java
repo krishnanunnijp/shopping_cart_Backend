055c049927264f204ecfa274f74e81fa14f1ad39
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-12-07 20:16:56 +0530

    add added

--- src/main/java/com/example/shopping_cart_Backend/controller/ShoppingControll.java
+++ src/main/java/com/example/shopping_cart_Backend/controller/ShoppingControll.java
@@ -4,8 +4,11 @@ import com.example.shopping_cart_Backend.dao.shoppingDao;
 import com.example.shopping_cart_Backend.model.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -15,9 +18,16 @@ public class ShoppingControll {
     @Autowired
     shoppingDao dao;
 
-    
+
     @GetMapping("/")
     public List<product> view(){
         return (List<product>) dao.findAll();
+    }
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public HashMap<String,String> add(@RequestBody product p){
+        HashMap<String,String> map=new HashMap<>();
+        dao.save(p);
+        map.put("status","success");
+        return map;
     }
 }
